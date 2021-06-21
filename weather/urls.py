f24f4b3f752d89849af1074856a0baa83a013e6e
diakonovmakar <diakonov.makar@gmail.com>
2021-06-21 09:35:08 +0200

    Updating caching

--- weather/urls.py
+++ weather/urls.py
@@ -1,11 +1,7 @@
-from django.conf import settings
-from django.core.cache.backends.base import DEFAULT_TIMEOUT
-from django.views.decorators.cache import cache_page
 from django.urls import path
 from .views import ForecastWeatherView
 
-CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 urlpatterns = [
-    path('', cache_page(CACHE_TTL)(ForecastWeatherView.as_view())),
+    path('', (ForecastWeatherView.as_view())),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,16 +1,23 @@
 from django.http import JsonResponse
 from django.views import View
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 from weather.services import get_weather, validate_date, validate_country_code
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
+
+@method_decorator(cache_page(CACHE_TTL), name='get')
 class ForecastWeatherView(View):
     def get(self, request):
         date = request.GET.get('date', '')
         country_code = request.GET.get('country_code', '')
 
         date_validation = validate_date(date)
-        code_validation = validate_country_code(country_code)        
+        code_validation = validate_country_code(country_code)
 
         if code_validation.success is True and date_validation.success is True:
             forecast = get_weather(date, country_code)
